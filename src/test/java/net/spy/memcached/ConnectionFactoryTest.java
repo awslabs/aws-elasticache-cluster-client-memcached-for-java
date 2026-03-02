d6312f3be01085baf1bb4983160d7e050df426cc
Artem Koshelev <akoshelev@atlassian.com>
2026-03-02 11:44:55 +1100

    ISSUE-55 revert accidentally deleted test

--- src/test/java/net/spy/memcached/ConnectionFactoryTest.java
+++ src/test/java/net/spy/memcached/ConnectionFactoryTest.java
@@ -49,6 +49,15 @@ public class ConnectionFactoryTest extends TestCase {
     new BinaryConnectionFactory(ClientMode.Static, 5, 5, DefaultHashAlgorithm.FNV1_64_HASH);
   }
 
+  public void testQueueSizes() {
+    ConnectionFactory cf = new DefaultConnectionFactory(100, 1024);
+    assertEquals(100, cf.createOperationQueue().remainingCapacity());
+    assertEquals(Integer.MAX_VALUE, cf.createWriteOperationQueue()
+        .remainingCapacity());
+    assertEquals(Integer.MAX_VALUE, cf.createReadOperationQueue()
+        .remainingCapacity());
+  }
+
   public void testDefaultConfigEndpointSelectionStrategy() {
     DefaultConnectionFactory cf = new DefaultConnectionFactory();
     assertTrue(cf.getConfigEndpointSelectionStrategy() instanceof RoundRobinConfigEndpointSelectionStrategy);
